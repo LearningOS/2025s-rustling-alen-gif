1658a826a933c85075326a4d12831a96a3152f79
root <root@LAPTOP-28FMLV83.>
2025-04-01 16:04:00 +0800

    solve macros1,2,3,4

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -3,19 +3,20 @@
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[rustfmt::skip]
-macro_rules! my_macro {
+macro_rules! my_macro1 {
     () => {
         println!("Check out my macro!");
     }
+}
+
+macro_rules! my_macro {
     ($val:expr) => {
         println!("Look at this other macro: {}", $val);
     }
 }
 
 fn main() {
-    my_macro!();
+    my_macro1!();
     my_macro!(7777);
 }
